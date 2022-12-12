f7a712a9dd4d307a6541fc70f91cb2e2534b2689
Luan-Pinheiro <luan.p12@homtail.com>
2022-12-12 20:29:08 -0300

    aplicação completa

--- averageCalculator.cpp
+++ averageCalculator.cpp
@@ -2,40 +2,100 @@
 using namespace std;
 #include<locale>
 
-void valoresCres(int a, int b);
+float mediaArit(float, float, float);
 
-void valoresDecres(int a, int b);
+float mediaPond(float, float, float, float, float, float);
+
+float mediaHarm(float, float, float);
 
 
 int main(){
 
-    int valor1, valor2;
+    float valor1, valor2, valor3, p1, p2, p3;
+    int selecao;
 
     setlocale(LC_ALL,"portuguese");
 
-    cout << "Insira o 1º valor \n";
-    cin >> valor1;
-    cout << "Insira o 2º valor \n";
-    cin >> valor2;
+    cout << "****CALCULADORA DE MEDIAS**** \n";
+    cout << "\n______________________________________________\n";
+    cout << "Insira o caractere respectivo a media desejada: \n";
+    cout << "1: Media Aritmetica \n2: Media Ponderada \n3: Media Harmonica \n0: Sair\n";
+    cout << "______________________________________________\n";
+    cin >> selecao;
+    while(selecao != 0){
 
-    !(valor1 > valor2) ? valoresCres(valor1, valor2) : valoresDecres(valor1, valor2);
+        switch(selecao){
+            case(1):
+                cout << "Insira o primeiro valor: \n";
+                cin >> valor1;
+                cout << "Insira o segundo valor: \n";
+                cin >> valor2;
+                cout << "Insira o terceiro valor: \n";
+                cin >> valor3;
+                cout << "Valor da Media Aritmetica: ";
+                cout << mediaArit(valor1, valor2, valor3);
+                break;
+            case(2):
+                cout << "Insira o primeiro valor: \n";
+                cin >> valor1;
+                cout << "Insira o primeiro peso: \n";
+                cin >> p1;
+                cout << "Insira o segundo valor: \n";
+                cin >> valor2;
+                cout << "Insira o segundo peso: \n";
+                cin >> p2;
+                cout << "Insira o terceiro valor: \n";
+                cin >> valor3;
+                cout << "Insira o terceiro peso: \n";
+                cin >> p3;
+                cout << "\nValor da Media Ponderada: ";
+                cout << mediaPond(valor1, valor2, valor3, p1, p2, p3);
+                break;
+            case(3):
+            cout << "Insira o primeiro valor: \n";
+                cin >> valor1;
+                cout << "Insira o segundo valor: \n";
+                cin >> valor2;
+                cout << "Insira o terceiro valor: \n";
+                cin >> valor3;
+                cout << "Valor da Media Harmonica: ";
+                cout << mediaHarm(valor1, valor2, valor3);
+                break;
+            default:
+                cout << "Entrada nao correspondente!";
+            }
+
+        cout << "\n\n______________________________________________\n";
+        cout << "Insira o caractere respectivo a mï¿½dia desejada: \n";
+        cout << "1: Media Aritmetica \n2: Media Ponderada \n3: Media Harmonica \n0: Sair\n\n";
+        cout << "______________________________________________\n";
+        cin >> selecao;
+    }
 
     return 0;
+}  
+float mediaArit(float a, float b, float c){
+    float valorMediaArit;
+
+    valorMediaArit = (a + b + c)/3;
+
+    return valorMediaArit;
 }
 
-void valoresCres(int a, int b){
-    if(a == b)
-        cout << "Valores iguais!\n";
-    else{
-        for( int i = a; i <= b; i++)
-            cout << i << " ";
-    }
+float mediaPond(float x, float y, float z, float px, float py, float pz){
+    float valorMediaPond;
+
+    valorMediaPond = (px*x + py*y + pz*z)/(px+py+pz);
+
+    return valorMediaPond;
 }
-void valoresDecres(int a, int b){
-    if(a == b)
-        cout << "Valores iguais!\n";
-    else{
-        for( int i = a; i >= b; i--)
-            cout << i << " ";
-    }
+
+float mediaHarm(float m, float n, float o){
+    float valorMediaHarm;
+
+    valorMediaHarm = 3 / ((1/m)+(1/n)+(1/o));
+
+    return valorMediaHarm;
 }
+
+
